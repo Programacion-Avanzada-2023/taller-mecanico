2331f10f324602efe68e701db6021237b705138d
leop <yukaleo43@gmail.com>
2023-10-08 13:52:37 -0300

    Modificacion de uso de querys en OrdenDeTrabajo

--- src/main/java/com/progavanzada/taller/mecanico/repositories/OrdenRepository.java
+++ src/main/java/com/progavanzada/taller/mecanico/repositories/OrdenRepository.java
@@ -3,8 +3,6 @@ package com.progavanzada.taller.mecanico.repositories;
 import com.progavanzada.taller.mecanico.entities.OrdenDeTrabajo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,9 +11,8 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface OrdenRepository extends JpaRepository<OrdenDeTrabajo, Integer> {
-    @Query("SELECT o FROM OrdenDeTrabajo o WHERE m.eliminado = false ORDER BY m.id")
-    public List<OrdenDeTrabajo> buscarTodo();
-   
-    @Query("SELECT o FROM OrdenDeTrabajo o WHERE m.eliminado = false AND m.id = :id")
-    public OrdenDeTrabajo buscarPorId(@Param("id") Integer id);
+    public List<OrdenDeTrabajo> findByEliminadoFalse();
+    
+    public OrdenDeTrabajo findByIdAndEliminadoFalse();
+    
 }
