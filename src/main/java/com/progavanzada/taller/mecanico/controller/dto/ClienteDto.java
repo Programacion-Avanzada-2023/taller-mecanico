654b865572cd70b58cb2db64bd3b308c8461f10d
Lucas <mazalucas@hotmail.com>
2023-10-08 18:48:19 -0300

    fix(*): correccion ubicacion dto de cliente

---
+++ src/main/java/com/progavanzada/taller/mecanico/controller/dto/ClienteDto.java
@@ -0,0 +1,13 @@
+package com.progavanzada.taller.mecanico.controller.dto;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ClienteDto {
+
+    public Integer id;
+    public PersonaDto person;
+    public AutomovilDto automovil;
+
+}
